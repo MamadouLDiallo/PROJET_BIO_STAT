22de55b923081a5f197732702633bcef6525276b
MamadouLDiallo <mamadoulamaranadiallomld1@gmail.com>
2025-01-02 20:50:33 +0000

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split as tts, GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
+import joblib  # Importer joblib pour charger le modèle
+from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import (
     accuracy_score,
     confusion_matrix,
@@ -27,7 +27,6 @@ def transform_variables(df):
         "Hypertension Arterielle", 
         "Diabete", 
         "Cardiopathie", 
-        #"hémiplégie",
         "Paralysie faciale", 
         "Aphasie", 
         "Hémiparésie", 
@@ -73,55 +72,38 @@ def main():
     # Séparation des données
     X_train, X_test, y_train, y_test = split(df_transformed)
 
-    #Paramètres de recherche par grille
-    st.subheader("Voici les meilleurs paramettres du modèle")
-    param_grid = {
-        'max_depth': [1, 5, 10, 20],
-        'min_samples_split': [2, 5, 10, 20],
-        'min_samples_leaf': [1, 2, 5, 10]
-    }
-    grid_search = GridSearchCV(estimator=DecisionTreeClassifier(), param_grid=param_grid, cv=5)
-    grid_search.fit(X_train, y_train)
-    best_params = grid_search.best_params_
+    # Charger le modèle pré-entraîné
+    model = None
+    try:
+        model = joblib.load("model.pkl")  # Assurez-vous que le fichier 'model.pkl' existe dans le répertoire
+        st.success("Modèle chargé avec succès !")
+    except Exception as e:
+        st.error(f"Erreur de chargement du modèle : {e}")
 
-    st.sidebar.subheader("Meilleurs paramètres")
-    st.write(best_params)
+    if model:
+        # Prédictions
+        y_pred = model.predict(X_test)
 
+        # Calculer les métriques de performance
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
 
-    #Entraîner le modèle avec les meilleurs paramètres
-    classifier = DecisionTreeClassifier(
-        max_depth=best_params["max_depth"],
-        min_samples_split=best_params["min_samples_split"],
-        min_samples_leaf=best_params["min_samples_leaf"],
-    )
+        # Afficher les résultats
+        st.subheader("Résultats")
+        st.write(f"**Accuracy**: {accuracy:.3f}")
+        st.write(f"**Précision**: {precision:.3f}")
+        st.write(f"**Recall**: {recall:.3f}")
 
-    classifier.fit(X_train, y_train)
-    import joblib
-    joblib.dump(classifier, "model1.pkl")
+        # Graphiques de performance avec un bouton "Exécuter"
+        graphes_perf = st.sidebar.multiselect(
+            "Graphiques de performance",
+            ["Confusion Matrix", "ROC Curve", "Precision-Recall Curve"]
+        )
+        execute = st.sidebar.button("Affichez les Graphiques")
 
-    # Prédictions
-    y_pred = classifier.predict(X_test)
-
-    # Calculer les métriques de performance
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-
-    # Afficher les résultats
-    st.subheader("Résultats")
-    st.write(f"**Accuracy**: {accuracy:.3f}")
-    st.write(f"**Précision**: {precision:.3f}")
-    st.write(f"**Recall**: {recall:.3f}")
-
-    # Graphiques de performance avec un bouton "Exécuter"
-    graphes_perf = st.sidebar.multiselect(
-        "Graphiques de performance",
-        ["Confusion Matrix", "ROC Curve", "Precision-Recall Curve"]
-    )
-    execute = st.sidebar.button("Affichez les Graphiques")
-
-    if execute:
-        plot_perf(graphes_perf, classifier, X_test, y_test)
+        if execute:
+            plot_perf(graphes_perf, model, X_test, y_test)
 
     # Formulaire pour les données du patient
     st.sidebar.header("Prédiction pour un Nouveau Patient")
@@ -134,7 +116,7 @@ def main():
         elif column == "Traitement":
             new_data[column] = st.sidebar.selectbox(f"{column} :", ["Thrombolyse", "Chirurgie"])
         elif column in [
-            "Hypertension Arterielle", "Diabete", "Cardiopathie", #"hémiplégie",
+            "Hypertension Arterielle", "Diabete", "Cardiopathie", 
             "Paralysie faciale", "Aphasie", "Hémiparésie", "Engagement Cerebral", "Inondation Ventriculaire"
         ]:
             new_data[column] = st.sidebar.radio(f"{column} :", ["OUI", "NON"])
@@ -176,21 +158,19 @@ def main():
     new_data_df = new_data_df[X_train.columns]
     # Bouton pour afficher le résultat de la prédiction
     if st.sidebar.button("Résultat de la Prédiction"):
-        prediction = classifier.predict(new_data_df)[0]
-        prediction_proba = classifier.predict_proba(new_data_df)[:, 1][0]
+        prediction = model.predict(new_data_df)[0]
+        prediction_proba = model.predict_proba(new_data_df)[:, 1][0]
         result = "Décédé" if prediction == 1 else "Vivant"
 
         st.subheader("Résultat de la Prédiction")
         st.write(f"résultat de la prédiction  : {prediction_proba:.0f}")
         st.write(f"Le modèle prédit que le patient est **{result}**.")
-        
-
 
 # Graphiques de performance
-def plot_perf(graphes, classifier, X_test, y_test):
+def plot_perf(graphes, model, X_test, y_test):
     if "Confusion Matrix" in graphes:
         st.subheader("Matrice de confusion")
-        y_pred = classifier.predict(X_test)
+        y_pred = model.predict(X_test)
         cm = confusion_matrix(y_test, y_pred)
         disp = ConfusionMatrixDisplay(confusion_matrix=cm)
         disp.plot(cmap="viridis")
@@ -198,7 +178,7 @@ def plot_perf(graphes, classifier, X_test, y_test):
 
     if "ROC Curve" in graphes:
         st.subheader("Courbe ROC")
-        y_pred_proba = classifier.predict_proba(X_test)[:, 1]
+        y_pred_proba = model.predict_proba(X_test)[:, 1]
         fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
         auc_score = roc_auc_score(y_test, y_pred_proba)
         plt.figure()
@@ -212,7 +192,7 @@ def plot_perf(graphes, classifier, X_test, y_test):
 
     if "Precision-Recall Curve" in graphes:
         st.subheader("Courbe Precision-Recall")
-        y_pred_proba = classifier.predict_proba(X_test)[:, 1]
+        y_pred_proba = model.predict_proba(X_test)[:, 1]
         precision, recall, _ = precision_recall_curve(y_test, y_pred_proba)
         plt.figure()
         plt.plot(recall, precision, label="Precision-Recall Curve")
@@ -221,7 +201,6 @@ def plot_perf(graphes, classifier, X_test, y_test):
         plt.title("Precision-Recall Curve")
         plt.legend()
         st.pyplot(plt.gcf())
-
 
 if __name__ == "__main__":
     main()
